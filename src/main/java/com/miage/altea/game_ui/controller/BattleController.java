4a7d52d1d4c6c8563e7600696512b961f36b6e97
mathieu <msansen@gmail.com>
2020-03-23 11:17:03 +0100

    fix trainer url

--- src/main/java/com/miage/altea/game_ui/controller/BattleController.java
+++ src/main/java/com/miage/altea/game_ui/controller/BattleController.java
@@ -1,11 +1,13 @@
 package com.miage.altea.game_ui.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
 
+@Controller
 public class BattleController {
 
     @GetMapping("/fight/{opponent}")
